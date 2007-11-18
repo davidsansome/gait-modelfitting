2dc35ccb767e8733d1071a324aaf2f8d2ee88938
David Sansome <me@davidsansome.com>
2007-11-18 18:09:43 +0000

    Normalise the filter
    git-svn-id: https://ssl.davidsansome.com/svn/3yp@38 f8f7d5d3-7237-0410-84b3-ada5caf6daf8

--- modelfitting/filters/PlotThighFilter.m
+++ modelfitting/filters/PlotThighFilter.m
@@ -6,18 +6,27 @@ cx = max(size(x));
 cy = max(size(y));
 cz = max(size(z));
 
+total = 0;
+data = zeros(cx, cy, cz);
+for nx = 1:cx
+    for ny = 1:cy
+        for nz = 1:cz
+            d = ThighFilter(x(nx), y(ny), nz/cz);
+            total = total + d;
+            data(nx, ny, nz) = d;
+        end
+    end
+end
+
 [file, message] = fopen('thigh.filter', 'w', 'ieee-be');
 fwrite(file, cx, 'uint8');
 fwrite(file, cy, 'uint8');
 fwrite(file, cz, 'uint8');
 
-data = zeros(cx, cy, cz);
 for nx = 1:cx
     for ny = 1:cy
         for nz = 1:cz
-            %data(nx, ny, nz) = ThighFilter(x(nx), y(ny), nz/cz);
-            d = ThighFilter(x(nx), y(ny), nz/cz);
-            fwrite(file, d, 'float32');
+            fwrite(file, data(nx, ny, nz) / total, 'float32');
         end
     end
 end
