dd31bcdf8322ce47c90eea7e031ae4c6cc82383f
David Sansome <me@davidsansome.com>
2007-11-19 00:56:01 +0000

    Change filter to output total at the end
    git-svn-id: https://ssl.davidsansome.com/svn/3yp@41 f8f7d5d3-7237-0410-84b3-ada5caf6daf8

--- modelfitting/filters/PlotThighFilter.m
+++ modelfitting/filters/PlotThighFilter.m
@@ -6,6 +6,11 @@ cx = max(size(x));
 cy = max(size(y));
 cz = max(size(z));
 
+[file, message] = fopen('thigh.filter', 'w', 'ieee-be');
+fwrite(file, cx, 'uint8');
+fwrite(file, cy, 'uint8');
+fwrite(file, cz, 'uint8');
+
 total = 0;
 data = zeros(cx, cy, cz);
 for nx = 1:cx
@@ -13,24 +18,15 @@ for nx = 1:cx
         for nz = 1:cz
             d = ThighFilter(x(nx), y(ny), nz/cz);
             total = total + d;
-            data(nx, ny, nz) = d;
+            %data(nx, ny, nz) = d;
+            fwrite(file, d, 'float32');
         end
     end
 end
 
-[file, message] = fopen('thigh.filter', 'w', 'ieee-be');
-fwrite(file, cx, 'uint8');
-fwrite(file, cy, 'uint8');
-fwrite(file, cz, 'uint8');
-
-for nx = 1:cx
-    for ny = 1:cy
-        for nz = 1:cz
-            fwrite(file, data(nx, ny, nz) / total, 'float32');
-        end
-    end
-end
-
+fwrite(file, total, 'float32');
 fclose(file);
 
 %contour(x,y,data(:,:,1))
+
+'Done'
