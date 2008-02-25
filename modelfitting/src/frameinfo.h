3f1858d6a8e2e1b5b287809074ba9106cdc790e8
David Sansome <me@davidsansome.com>
2008-02-25 22:25:10 +0000

    Split the Params class into its own file
    git-svn-id: https://ssl.davidsansome.com/svn/3yp@111 f8f7d5d3-7237-0410-84b3-ada5caf6daf8

--- modelfitting/src/frameinfo.h
+++ modelfitting/src/frameinfo.h
@@ -2,6 +2,7 @@
 #define FRAMEINFO_H
 
 #include "types.h"
+#include "params.h"
 
 #include <QFuture>
 #include <QMetaType>
@@ -43,95 +44,6 @@ enum Part
 	LeftLeg,
 	RightLeg
 };
-
-template <typename T>
-class Params
-{
-public:
-	Params() : valid(false) {}
-	Params(T ta, T tt, T la, T lt);
-	Params(const Params& other);
-	Params(QSettings& settings);
-	
-	bool operator <(const Params& other) const;
-	Params& operator =(const Params& other);
-	
-	void save(QSettings& settings) const;
-	
-	bool valid;
-	T thighAlpha;
-	T thighTheta;
-	T lowerLegAlpha;
-	T lowerLegTheta;
-};
-
-template <typename T>
-QDebug operator <<(QDebug s, const Params<T>& p)
-{
-	s.nospace() << "Params(" << p.thighAlpha << ", " << p.thighTheta << ", " << p.lowerLegAlpha << ", " << p.lowerLegTheta << ")";
-	return s.space();
-}
-
-template <typename T>
-Params<T>::Params(T ta, T tt, T la, T lt)
-	: valid(true),
-	  thighAlpha(ta),
-	  thighTheta(tt),
-	  lowerLegAlpha(la),
-	  lowerLegTheta(lt)
-{
-}
-
-template <typename T>
-Params<T>::Params(const Params& other)
-{
-	*this = other;
-}
-
-template <typename T>
-Params<T>::Params(QSettings& settings)
-	: valid(true)
-{
-	thighAlpha = settings.value("ThighAlpha", 0.0).value<T>();
-	thighTheta = settings.value("ThighTheta", 0.0).value<T>();
-	lowerLegAlpha = settings.value("LowerLegAlpha", 0.0).value<T>();
-	lowerLegTheta = settings.value("LowerLegTheta", 0.0).value<T>();
-}
-
-template <typename T>
-Params<T>& Params<T>::operator =(const Params& other)
-{
-	valid = other.valid;
-	thighAlpha = other.thighAlpha;
-	thighTheta = other.thighTheta;
-	lowerLegAlpha = other.lowerLegAlpha;
-	lowerLegTheta = other.lowerLegTheta;
-	return *this;
-}
-
-template <typename T>
-bool Params<T>::operator <(const Params& other) const
-{
-	if (thighAlpha != other.thighAlpha)
-		return thighAlpha < other.thighAlpha;
-	if (thighTheta != other.thighTheta)
-		return thighTheta < other.thighTheta;
-	if (lowerLegAlpha != other.lowerLegAlpha)
-		return lowerLegAlpha < other.lowerLegAlpha;
-	if (lowerLegTheta != other.lowerLegTheta)
-		return lowerLegTheta < other.lowerLegTheta;
-	return false;
-}
-
-template <typename T>
-void Params<T>::save(QSettings& settings) const
-{
-	settings.setValue("ThighAlpha", thighAlpha);
-	settings.setValue("ThighTheta", thighTheta);
-	settings.setValue("LowerLegAlpha", lowerLegAlpha);
-	settings.setValue("LowerLegTheta", lowerLegTheta);
-}
-
 
 class MapType
 {
