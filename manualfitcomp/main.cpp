835f8c51d6ec481b2e7f7fc5800ccbaf23e3bc1f
David Sansome <me@davidsansome.com>
2008-04-20 14:26:14 +0000

    Manual fitting
    git-svn-id: https://ssl.davidsansome.com/svn/3yp@226 f8f7d5d3-7237-0410-84b3-ada5caf6daf8

---
+++ manualfitcomp/main.cpp
@@ -0,0 +1,104 @@
+#include <QApplication>
+#include <QtDebug>
+#include <QFile>
+#include <QList>
+#include <QIODevice>
+#include <QStringList>
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void readFile(QIODevice& file, QList<float>& leftThigh, QList<float>& leftLower, QList<float>& rightThigh, QList<float>& rightLower)
+{
+	while (!file.atEnd())
+	{
+		QString line(file.readLine());
+		line = line.trimmed();
+		
+		if (line.isEmpty())
+			break;
+		
+		QStringList values(line.split(" "));
+		Q_ASSERT(values.size() == 9);
+		
+		bool ok;
+		leftThigh << values[2].toFloat(&ok);    Q_ASSERT(ok == true);
+		leftLower << values[4].toFloat(&ok);    Q_ASSERT(ok == true);
+		rightThigh << values[6].toFloat(&ok);   Q_ASSERT(ok == true);
+		rightLower << values[8].toFloat(&ok);   Q_ASSERT(ok == true);
+	}
+}
+
+QPair<float, float> compare(const QString& label, const QList<float>& a, const QList<float>& m)
+{
+	Q_ASSERT(a.count() == m.count());
+	
+	float diff = 0.0;
+	float diffSquared = 0.0;
+	
+	for (int i=0 ; i<a.count() ; ++i)
+	{
+		float d = qAbs(a[i] - m[i]);
+		
+		diff += d;
+		diffSquared += pow(d, 2);
+	}
+	
+	cout << qPrintable(label) << " & " << diff << " & " << diffSquared << " \\\\" << endl;
+	
+	return qMakePair(diff, diffSquared);
+}
+
+int main(int argc, char** argv)
+{
+	QApplication app(argc, argv, false);
+	
+	// Check our arguments
+	if (argc != 3)
+		qFatal("Usage: %s autofit.dat manualfit.dat\n", argv[0]);
+	
+	QFile autoFile(argv[1]);
+	QFile manualFile(argv[2]);
+	
+	if (!autoFile.exists())
+		qFatal("%s does not exist\n", qPrintable(autoFile.fileName()));
+	
+	if (!manualFile.exists())
+		qFatal("%s does not exist\n", qPrintable(manualFile.fileName()));
+	
+	
+	// Read the files
+	QList<float> autoLeftThigh;
+	QList<float> autoLeftLower;
+	QList<float> autoRightThigh;
+	QList<float> autoRightLower;
+	QList<float> manualLeftThigh;
+	QList<float> manualLeftLower;
+	QList<float> manualRightThigh;
+	QList<float> manualRightLower;
+	
+	autoFile.open(QIODevice::ReadOnly);
+	manualFile.open(QIODevice::ReadOnly);
+	
+	readFile(autoFile, autoLeftThigh, autoLeftLower, autoRightThigh, autoRightLower);
+	readFile(manualFile, manualLeftThigh, manualLeftLower, manualRightThigh, manualRightLower);
+	
+	// Compare the values
+	float diff = 0.0;
+	float diffSquared = 0.0;
+	QPair<float, float> ret;
+	
+	ret = compare("Left thigh", autoLeftThigh, manualLeftThigh);         diff += ret.first;  diffSquared += ret.second;
+	ret = compare("Right thigh", autoRightThigh, manualRightThigh);      diff += ret.first;  diffSquared += ret.second;
+	ret = compare("Left lower leg", autoLeftLower, manualLeftLower);     diff += ret.first;  diffSquared += ret.second;
+	ret = compare("Right lower leg", autoRightLower, manualRightLower);  diff += ret.first;  diffSquared += ret.second;
+	
+	cout << "\\hline" << endl;
+	cout << "Total & " << diff << " & " << diffSquared << " \\\\" << endl;
+	
+	return 0;
+}
+
